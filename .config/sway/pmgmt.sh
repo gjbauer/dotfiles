c1c976947300000322cfa0d3896f071768f38db2
Gabriel Bauer <gabeb1277@gmail.com>
2026-01-12 14:07:36 -0500

    updated config

--- .config/sway/pmgmt.sh
+++ .config/sway/pmgmt.sh
@@ -1,5 +1,14 @@
-!/bin/sh
-swaylock \
+#!/bin/sh
+
+# Anti-cold boot measure:
+# Ensure that after the screen locks,
+# if it is not unlocked within 5 minutes
+# shutdown.
+#
+# This is needed even with TPM as researchers
+# have overwritten the MOR bit in UEFI firmware
+# to bypass zeroization.
+LOCK_COMMAND="swaylock \
 	--screenshots \
 	--clock \
 	--indicator \
@@ -13,4 +22,10 @@ swaylock \
 	--inside-color 00000088 \
 	--separator-color 00000000 \
 	--grace 2 \
-	--fade-in 0.2
+	--fade-in 0.2"
+
+timeout 5m $LOCK_COMMAND
+
+if [ $? -eq 124 ]; then
+	doas poweroff
+fi
