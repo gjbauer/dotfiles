008eb41bf910021c08748ae60e1e104949a9e359
gjbauer <gabeb1277@gmail.com>
2026-01-25 15:02:55 -0500

    Refactor sway-obsd-setup.sh to use doas
    Updated script to use 'doas' instead of 'su' for user modifications and service management.

--- .scripts/sway-obsd-setup.sh
+++ .scripts/sway-obsd-setup.sh
@@ -1,20 +1,11 @@
 #!/bin/sh
 
-echo "Make sure to run using 'source sway-obsd-setup.sh'"
-
 # Get user information before entering superuser mode
 user=$(whoami)
 id=$(id -u)
 
-# Enter superuser
-su
-
 # Add user to seatd group
-usermod -G _seatd $user
+doas usermod -G _seatd $user
 
 # Enable seatd
-rcctl enable seatd
-
-exit
-
-return 0
+doas rcctl enable seatd
