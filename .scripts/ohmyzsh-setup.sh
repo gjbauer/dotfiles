8195a063061d1d69f3abee790c522362ea109039
gjbauer <gabeb1277@gmail.com>
2026-01-25 14:39:22 -0500

    Replace fetch with curl for Oh My Zsh setup

--- .scripts/ohmyzsh-setup.sh
+++ .scripts/ohmyzsh-setup.sh
@@ -1,1 +1,1 @@
-sh -c "$(fetch -o - https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
