c2f48a24001f9d8b96c5670c7388d6bb89b92776
gjbauer <gabeb1277@gmail.com>
2026-05-28 20:07:56 -0400

    Simplify lock command in pmgmt.sh
    Simplified the lock command to just 'swaylock'.

--- .config/sway/pmgmt.sh
+++ .config/sway/pmgmt.sh
@@ -8,21 +8,7 @@
 # This is needed even with TPM as researchers
 # have overwritten the MOR bit in UEFI firmware
 # to bypass zeroization.
-LOCK_COMMAND="swaylock \
-	--screenshots \
-	--clock \
-	--indicator \
-	--indicator-radius 100 \
-	--indicator-thickness 7 \
-	--effect-blur 7x5 \
-	--effect-vignette 0.5:0.5 \
-	--ring-color bb00cc \
-	--key-hl-color 880033 \
-	--line-color 00000000 \
-	--inside-color 00000088 \
-	--separator-color 00000000 \
-	--grace 2 \
-	--fade-in 0.2"
+LOCK_COMMAND="swaylock"
 
 timeout 5m $LOCK_COMMAND
 
